b255d6ac9dabd2a32282df030d4d938cae4f1e36
adityabisoi <adityabisoi1999@gmail.com>
2019-12-10 13:38:44 +0530

    Speech Recognition

--- contentScript.js
+++ contentScript.js
@@ -1,11 +1,12 @@
+const textArea = document.getElementsByTagName('textarea')[0];
+
 var btn = createButton();
 document.addEventListener('focusin', onFocusIn);
 
 function onFocusIn(event) {
     el = event.target;
     if(el.type != "button" && el.type != "submit" ) {
-        if (
-            el.matches(' textarea')) // el.contentEditable ||;&& el.type.match(/email|number|search|text|url/)
+        if (el.matches('textarea')) // el.contentEditable ||;&& el.type.match(/email|number|search|text|url/)
         {
             appendButton(el);
         }
@@ -16,7 +17,43 @@ function createButton() {
     var btn = document.createElement('button');
     btn.textContent = 'Yay!';
     btn.onclick = function(event) {
-        btn.textContent = 'clicked';
+        // textArea.textContent = 'clicked';
+
+        window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+        const recognition = new SpeechRecognition();
+        recognition.interimResults = true;
+
+        // let p= document.createElement('p');
+        // const words= document.querySelector('.words');
+        // words.appendChild(p);
+
+        recognition.addEventListener('result', e=>{
+            const transcript = Array.from(e.results)
+                .map(result=> result[0])
+                .map(result=>result.transcript)
+                .join('')
+            // p.textContent = transcript;
+            // if(e.results[0].isFinal)
+            // {
+            //     p = document.createElement('p');
+            //     words.appendChild(p);
+            // }
+            // if(transcript.includes('get weather'))
+            // {
+            //     alert("Getting weather");
+            // }
+
+            textArea.textContent = transcript;
+
+            console.log(transcript);
+        });
+
+        recognition.addEventListener('end', recognition.start);
+
+        recognition.start();
+
+
     };
     return btn;
 }
